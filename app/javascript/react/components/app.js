fdf4c5e475080ae40f414e6f1bc53803c8d38b9f
Jack Marston <jackdyett@gmail.com>
2018-12-12 09:46:37 -0600

    set up base data and visual components

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import initialData from '../src/initialData'
+import Column from './column'
 
-export const App = (props) => {
-  return (<h1>Make It So React</h1>)
+class App extends React.Component {
+  state = initialData;
+
+  render() {
+    const columns = this.state.columnOrder.map((columnId) => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+      return (
+        <Column key={column.id} column={column} task={tasks} />
+      )
+    })
+    return (
+      <div>
+        {columns}
+      </div>
+    )
+  }
 }
+
 
 export default App
