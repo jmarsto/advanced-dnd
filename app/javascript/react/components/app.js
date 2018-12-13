578d0e3acbd938e3856df538c4435cf3c5a35e22
Jack Marston <jackdyett@gmail.com>
2018-12-13 14:13:11 -0600

    update onDragEnd to drag between columns

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -17,26 +17,56 @@ class App extends React.Component {
       return;
     }
 
-    const column = this.state.columns[source.droppableId];
-    const newTaskIds = Array.from(column.taskIds);
+    const startColumn = this.state.columns[source.droppableId];
+    const finishColumn = this.state.columns[destination.droppableId];
 
-    newTaskIds.splice(source.index, 1);
-    newTaskIds.splice(destination.index, 0, draggableId);
+    if (startColumn === finishColumn) {
+      const newTaskIds = Array.from(startColumn.taskIds);
+      newTaskIds.splice(source.index, 1);
+      newTaskIds.splice(destination.index, 0, draggableId);
 
-    const newColumn = {
-      ...column,
-      taskIds: newTaskIds
+      const newColumn = {
+        ...startColumn,
+        taskIds: newTaskIds
+      }
+
+      const newState = {
+        ...this.state,
+        columns: {
+          ...this.state.columns,
+          [newColumn.id]: newColumn
+        }
+      }
+
+      this.setState(newState);
+      return;
+    }
+
+    const startColumnTaskIds = Array.from(startColumn.taskIds);
+    startColumnTaskIds.splice(source.index, 1);
+    const newStartColumn = {
+      ...startColumn,
+      taskIds: startColumnTaskIds
+    }
+
+    const finishColumnTaskIds = Array.from(finishColumn.taskIds);
+    finishColumnTaskIds.splice(destination.index, 0, draggableId);
+    const newFinishColumn = {
+      ...finishColumn,
+      taskIds: finishColumnTaskIds
     }
 
     const newState = {
       ...this.state,
       columns: {
         ...this.state.columns,
-        [newColumn.id]: newColumn
+        [newStartColumn.id]: newStartColumn,
+        [newFinishColumn.id]: newFinishColumn
       }
     }
 
     this.setState(newState);
+    return;
   };
 
   render() {
